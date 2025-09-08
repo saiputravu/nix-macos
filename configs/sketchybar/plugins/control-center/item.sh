d4c9106a4ef4fae0b3d208b12b9d01264494cf87
sai <aputravu1234@gmail.com>
2025-09-08 19:24:34 +0100

    sketchybar

---
+++ configs/sketchybar/plugins/control-center/item.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source "$PLUGIN_DIR/battery/item.sh"
+source "$PLUGIN_DIR/wifi/item.sh"
+source "$PLUGIN_DIR/bluetooth/item.sh"
+source "$PLUGIN_DIR/volume/item.sh"
+
+source "$PLUGIN_DIR/control-center/items/divider.sh"
